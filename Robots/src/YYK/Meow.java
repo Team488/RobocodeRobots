7633a5ecbc8fc9d03878098080b43996d4b0ea4d
YuenKwong <yuenyingk@gmail.com>
2015-08-22 14:01:31 -0700

    Beat Tracker

--- Robots/src/YYK/Meow.java
+++ Robots/src/YYK/Meow.java
@@ -20,7 +20,7 @@ public class Meow extends AdvancedRobot{
 	}
 	public void onScannedRobot(ScannedRobotEvent e) {
 	    setTurnRight(20);   
-		setFire(2);
+		setFire(4);
 	        //ahead(100);
 		execute();
    
@@ -28,6 +28,18 @@ public class Meow extends AdvancedRobot{
 		public void onHitRobot(HitRobotEvent event) {
 		       if (event.getBearing() > -90 && event.getBearing() <= 90) {
 		         back(100);
+		         
+		      // Set the gun to turn 180 degrees to the left
+		         setTurnGunLeft(180);
+
+		         // Set the gun to turn 90 degrees to the right instead of left
+		         // (overrides the previous order)
+		         setTurnGunLeft(-90);
+
+		         
+		         // Executes the last setTurnGunLeft()
+		         execute();
+		       
 		       }
 		
 		
